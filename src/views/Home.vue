4460103bde5b8f4a7f8ae18a68b580f053bc3fe0
Trevor Capozza <trevorcapozza@trevorsMBP.attlocal.net>
2021-04-16 12:08:51 -0700

    more changes

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,6 +3,23 @@
     <div class="hero">
       <h1>Start earning passive income today.</h1>
     </div>
+    <div class="section-one">
+      <div class="wrapper">
+      <h2>What We Do</h2>
+      <p>Project Passive was created to provide its clients with a stream of passive income.
+        We employ a team that automates the entire dropshipping flow between Amazon, Walmart, and Shopify.
+        <br><br>We are here to help you with every step of the journey. We take care of everything from LLC creation
+        to getting through the rigorous application process with Walmart and Amazon.</p>
+      </div>
+    </div>
+    <div class="section-two">
+      <h2>Dropshipping Partners</h2>
+      <div class="logo-row">
+        <img class="drop-logo" src="@/assets/amazon_logo.svg" alt="Amazon Logo">
+        <img class="drop-logo" src="@/assets/shopify-logo.svg" alt="Shopify Logo">
+        <img class="drop-logo" src="@/assets/walmart_logo.svg" alt="Walmart Logo">
+      </div>
+    </div>
     <Contact/>
   </div>
 </template>
