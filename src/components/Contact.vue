e953001ca49f7c3333198dc70fe4e2923350c6f4
Trevor Capozza <trevorcapozza@trevorsMBP.attlocal.net>
2021-05-25 17:37:08 -0700

    form bugging

--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -2,7 +2,7 @@
 <div class="contact max">
     <h2>Contact</h2>
     <form name="Contact" method="POST" data-netlify="true" data-netlify-honeypot="bot-field">
-      <input type="hidden" name="form-name" value="contact" />
+      <input type="hidden" name="form-name" value="Contact" />
         <input type="text" name="name" placeholder="Name"/>   
         <input type="email" name="email" placeholder="Email"/>
         <textarea name="message" placeholder="Message"></textarea>
